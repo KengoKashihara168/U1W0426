3024aa6aa16b66a5c1e3a3735a98b54b25f86d31
Kengo Kashihara <KengoKashihara1998@gmail.com>
2021-04-27 07:43:53 +0900

    ゲームマネージャーを追加
    ゲームマネージャーのスクリプトを追加

---
+++ RowBoat/Assets/GameScene/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Player _player = null; // プレイヤー
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _player.UpdatePlayer();
+    }
+}
