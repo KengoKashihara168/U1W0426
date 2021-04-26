fba807a780ed6702c90da569a64294c3f62448cf
Kengo Kashihara <KengoKashihara1998@gmail.com>
2021-04-27 07:44:29 +0900

    プレイヤーを追加
    プレイヤーのスクリプトを追加

---
+++ RowBoat/Assets/GameScene/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public void Initialize()
+    {
+        Debug.Log("プレイヤーの初期化");
+    }
+
+    public void UpdatePlayer()
+    {
+        Debug.Log("プレイヤーの更新");
+    }
+}
