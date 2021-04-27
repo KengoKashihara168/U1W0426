b6d2c6b34d658a23d5e2f85e4d1895d81d45db74
Kengo Kashihara <KengoKashihara1998@gmail.com>
2021-04-28 00:32:56 +0900

    ボタンでプレイヤーが移動するように実装
    ボタンを押すことでプレイヤーが左右に移動するように実装

--- RowBoat/Assets/GameScene/GameManager.cs
+++ RowBoat/Assets/GameScene/GameManager.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private Player _player = null; // プレイヤー
+    [SerializeField] private ButtonManager  _buttonManager   = null; // ボタンマネージャー
+    [SerializeField] private Player         _player          = null; // プレイヤー
+
+    public delegate void ArrowClickedFunc(ButtonManager.ArrowType arrow); // 矢印が押されたときのデリゲート
 
     // Start is called before the first frame update
     void Start()
     {
         _player.Initialize();
+        _buttonManager.SetArrowClickedFunc(ArrowClicked);
     }
 
     // Update is called once per frame
@@ -19,6 +23,23 @@ public class GameManager : MonoBehaviour
 
         // プレイヤーの移動
         float x = Input.GetAxis("Horizontal");
-        _player.Move(new Vector3(x,0.0f,0.0f));
+        
+    }
+
+    /// <summary>
+    /// 矢印が押された
+    /// </summary>
+    /// <param name="arrow">押された矢印の種類</param>
+    private void ArrowClicked(ButtonManager.ArrowType arrow)
+    {
+        switch(arrow)
+        {
+            case ButtonManager.ArrowType.Left:
+            _player.Move(new Vector3(-1,0.0f,0.0f));
+            break;
+            case ButtonManager.ArrowType.Right:
+            _player.Move(new Vector3(1,0.0f,0.0f));
+            break;
+        }
     }
 }
